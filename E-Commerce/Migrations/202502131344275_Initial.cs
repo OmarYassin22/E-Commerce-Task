92ed400c89bcdc8ef4ef6eb730ae7ed800eb7d02
OmarYassin22 <o.y.sleem@gmail.com>
2025-02-13 15:45:20 +0200

    intial migrate

---
+++ E-Commerce/Migrations/202502131344275_Initial.cs
@@ -0,0 +1,47 @@
+﻿namespace E_Commerce.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        FirstNme = c.String(),
+                        LastNme = c.String(),
+                        Address = c.String(),
+                        Email = c.String(),
+                        Phone = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                        Price = c.Single(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Customer_Id = c.Guid(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Customers", t => t.Customer_Id)
+                .Index(t => t.Customer_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Orders", "Customer_Id", "dbo.Customers");
+            DropIndex("dbo.Orders", new[] { "Customer_Id" });
+            DropTable("dbo.Orders");
+            DropTable("dbo.Customers");
+        }
+    }
+}
